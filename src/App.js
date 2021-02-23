1e5dbed5633159c17c38ba881aeadc59ef37011f
azizsheq <azizsheq@gmail.com>
2021-02-23 10:15:47 +0800

    updating

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Dr Mahfuz",
+    job: "Singer"
+  }
+  var style = {
+    color: "green",
+    backgroundColor: "yellow"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit DONE ! <code>src/App.js</code> and save to reload.
+        <h1 className="" style={style}>My Heading</h1>
+        <h2>Printing Object : {person.name}, job is {person.job}</h2>
+        <h2>Doing Sum : {22+33+44+55}</h2>
+        <p style={{backgroundColor: 'tomato', color: "yellow", fontSize: '50px'}}>
+          My First React Paragraph
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
